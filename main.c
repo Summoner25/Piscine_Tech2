1336a880671d54fdfceffb74b687c392a4ce717a
Summoner25 <courciermarvin@gmail.com>
2015-01-07 14:11:25 +0100

    ajout main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+** main.c for main.c in /home/courci_b/Tech2/Piscine_Tech2
+** 
+** Made by Courcier Marvin
+** Login   <courci_b@epitech.net>
+** 
+** Started on  Wed Jan  7 14:10:52 2015 Courcier Marvin
+** Last update Wed Jan  7 14:11:02 2015 Courcier Marvin
+*/
+
+int	main(int ac, char **av)
+{
+}
